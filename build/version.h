e593039a2d40eeac9a258b06895bb95282214193
SergKekuh <serg.kekuh@gmail.com>
2024-01-04 17:00:33 +0200

    Not wotk create for home work

--- build/version.h
+++ build/version.h
@@ -1,3 +1,13 @@
 #pragma once
 
+#include <string.h>
+ 
+#include <boost/cstdint.hpp>
+ 
+ namespace Version
+{
+	const std::string& AsText();
+	boost::uint64_t AsNumber();
+}
+
 #define PROJECT_VERSION_PATCH 1
