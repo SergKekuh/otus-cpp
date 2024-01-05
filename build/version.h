57ce94b30178ec77bd4f813993223d9b0e09b936
SergKekuh <serg.kekuh@gmail.com>
2024-01-05 12:21:23 +0200

    Delete file cpp version

--- build/version.h
+++ build/version.h
@@ -1,13 +1,4 @@
 #pragma once
 
-#include <string.h>
- 
-#include <boost/cstdint.hpp>
- 
- namespace Version
-{
-	const std::string& AsText();
-	boost::uint64_t AsNumber();
-}
 
 #define PROJECT_VERSION_PATCH 1
