8d3860044236db32d8df97a0be9297a29431763c
SergKekuh <serg.kekuh@gmail.com>
2024-01-19 16:26:25 +0200

    Google test version repair 1

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include "lib.h"
 
+#include "gtest/gtest.h"
 #include <iostream>
 
 int main (int, char **) 
 {
+    ::testing::InitGoogleTest(version());
     std::cout << "Version: " << version() << std::endl;
     std::cout << "Hello, world!" << std::endl;
-    return 0;
+    return RUN_ALL_TESTS();
 }
