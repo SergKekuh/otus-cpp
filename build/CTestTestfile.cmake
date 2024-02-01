8c1c15cbaecb67c50422bdc3c91e1914755f0c2f
SergKekuh <serg.kekuh@gmail.com>
2024-02-01 16:52:43 +0200

    Cmake google changes

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/serg/Documents/otus-cpp
+# Build directory: /home/serg/Documents/otus-cpp/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[test_version]=] "test_version")
+set_tests_properties([=[test_version]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/serg/Documents/otus-cpp/CMakeLists.txt;87;add_test;/home/serg/Documents/otus-cpp/CMakeLists.txt;0;")
+subdirs("build/_deps/googletest-src")
